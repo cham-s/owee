700b174f0d47cb2a789e68b050538c772b35797e
dash-c <deangreyhame@gmail.com>
2014-09-07 22:42:26 +0200

    add owee and open browser correctly

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,12 @@
 # the dashboard display all the owee transaction
 
 import webbrowser
+import ower
+import time
+from time import gmtime
+import csv
 import os
+
 
 
 # declare the dashboard header
@@ -18,6 +23,7 @@ main_page_head ='''
     	padding-top: 70px;
     }
 </style>
+<link rel="stylesheet" type="text/css" href="css/dashboard.css">
 </head>
 '''
 
@@ -27,70 +33,139 @@ main_page_content = '''
 <!DOCTYPE html>
 <html lang="en">
 
-</head>
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-2">nothing</div>
-			<div class="col-md-10">
-				<table class="table table-striped">
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Stuff</th>
-				<th>Start Date</th>
-				<th>Due Date</th>
-				<th>Status</th>
-				<th>Actions</th>
-			</tr>
-			<tr>
-		</thead>
-		<tbody>
-			{ower_rows}
-		</tbody>	
-	</table>
-			</div>
-		</div>
-	</div>
-	<script src="js/myscript.js">
-</body>
+  
+  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Owee</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="col-md-12 col-lg-12">
+            <h1 class="page-header">Overview</h1>
+
+            <div class="row placeholders">
+              <div class="col-xs-6 col-sm-3 placeholder">
+                <img data-src="holder.js/200x200/auto/sky" src="img/simple.png" class="img-responsive" alt="Generic placeholder thumbnail">
+                <h4>Label</h4>
+                <span class="text-muted">Something else</span>
+              </div>
+              <div class="col-xs-6 col-sm-3 placeholder">
+                <img data-src="holder.js/200x200/auto/vine" src="img/6charts.png" class="img-responsive" alt="Generic placeholder thumbnail">
+                <h4>Label</h4>
+                <span class="text-muted">Something else</span>
+              </div>
+              <div class="col-xs-6 col-sm-3 placeholder">
+                <img data-src="holder.js/200x200/auto/sky" src="img/simple.png" class="img-responsive" alt="Generic placeholder thumbnail">
+                <h4>Label</h4>
+                <span class="text-muted">Something else</span>
+              </div>
+              <div class="col-xs-6 col-sm-3 placeholder">
+                <img data-src="holder.js/200x200/auto/vine" src="img/6charts.png" class="img-responsive" alt="Generic placeholder thumbnail">
+                <h4>Label</h4>
+                <span class="text-muted">Something else</span>
+              </div>
+            </div>
+
+            <h2 class="sub-header">Owees</h2>
+            <div class="table-responsive">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Object Type</th>
+                    <th>Value</th>
+                    <th>Start date</th>
+                    <th>Due date</th>
+                    <th>Status</th>
+                    <th>Note</th>
+                  </tr>
+                  </thead>
+                  <tbody>   
+
+                  {owee_rows}
+                  
+                </tbody>  
+              </table>
+
+            </div>
+          </div>
+        </div>
+    </div>
+
+  </body>
 </html>
 '''
 
 # ower row template
-ower_row = '''
+owee_row = '''
 <tr>
-	<td>{ower_name}</td>
-	<td>{lended_stuff}</td>
+	<td>{owee_name}</td>
+	<td>{object_type}</td>
+	<td>${value}</td>
 	<td>{start_date}</td>
 	<td>{end_date}</td>
 	<td>{status}</td>
-	<td><button type="button" class="btn btn-primary">Primary</button><span> </span><button type="button" class="btn btn-success">Success</button></td>
-</tr>
+	<td>{note}</td>
+<tr>
 '''
+
+# add csv line
+def add_line():
+	f = open("owees.csv", "a")
+	name = str(raw_input("Enter name: ")).strip()
+	email = str(raw_input("Enter email: ")).strip()
+	object_type = str(raw_input("Enter type: ")).strip()
+	value = str(raw_input("Enter value: ")).strip()
+	start_date = time.strftime(" %d %b %Y", gmtime())
+	end_date = str(raw_input("Enter a due date: ")).strip()
+	owee_status = "Still lended"
+	comment = str(raw_input("Enter a note regarding the transaction: ")).strip()
+
+	row = [name, email, object_type, value, start_date, end_date, owee_status, comment]
+	csv_line = ", ".join(row)
+	f.write(csv_line + '\n')
+
+	f.close()
+
+
 # create a single row fill with dynamic content
-def create_ower_row_content(owers):
+def create_ower_row_content(owees):
 	# create rows content
 	content = ''
-	for ower in owers:
+	for owee in owees:
 		# append with previous entry
-		content += ower_row.format(
-		ower_name=ower.name,
-		lended_stuff=ower.stuff,
-		start_date=ower.start,	
-		end_date=ower.end,
-		status=ower.status
+		content += owee_row.format(
+		owee_name=owee.name,
+		object_type=owee.type,
+		value=owee.value,
+		start_date=owee.start,	
+		end_date=owee.end,
+		status=owee.status,
+		note=owee.comment
 		) 
 	return content	
 
 # open a webbrowser with all owees	
 
-def open_dashbord_page(owers):
+def open_dashbord_page(owees):
 	# create or overwrite the ouput file
 	output_file = open('dashboard.html', 'w')
 
 	# replace the placeholder for the owee with dynamic content
-	rendered_content = main_page_content.format(ower_rows=create_ower_row_content(owers))
+	rendered_content = main_page_content.format(owee_rows=create_ower_row_content(owees))
 
 	#output the file
 	output_file.write(main_page_head + rendered_content)
@@ -100,7 +175,25 @@ def open_dashbord_page(owers):
 	url = os.path.abspath(output_file.name)
 	webbrowser.open('file://' + url, new=2) # open in a new tab if possible
 
-#
+# add a row and open the webpage with new 
+def add_owee():
+	add_line()
+	owees = []
+	csv_list = []	
+
+	with open('owees.csv', 'rb') as csvfile:
+		reader = csv.reader(csvfile, delimiter = ',')
+		for row in reader:
+	   		csv_list.append(row)
+
+	for row in csv_list:
+		owees.append(ower.Owee(row))
+	open_dashbord_page(owees)
+	
+
+
+
+
 
 
 
--- ower.py
+++ ower.py
@@ -4,18 +4,23 @@
 
 import time
 from time import gmtime
+import csv
 
 # define ower 
 
+#def __init__(self, csv_file): reader = csv.reader(csv_file); and then assign to self.name, self.address
+
 class Owee():
 	"""This class define all the attribute of an ower the person we lend the stuff to"""
-	def __init__(self, ower_name, ower_email, lended_stuff, end_date):
-		self.name = ower_name
-		self.email = ower_email
-		self.stuff = lended_stuff
-		self.start = time.strftime("%a, %d %b %Y", gmtime())
-		self.end = end_date
-		self.status = "Still Lended"
+	def __init__(self, array_of_value):
+		self.name = array_of_value[0]
+		self.email = array_of_value[1]
+		self.type = array_of_value[2]
+		self.value = array_of_value[3]
+		self.start = array_of_value[4]
+		self.end = array_of_value[5]
+		self.status = array_of_value[6]
+		self.comment = array_of_value[7]
 
 		# this method send an email to the ower to kindly remind him of the stuff he borrowed
 	#def reminder():
@@ -24,6 +29,13 @@ class Owee():
 	def change_status(self):
 		if self.status == "Still Landed":
 			self.status = "Given back"
+
+	# this method return array of all the owee propreties	
+	def generate_row(self):
+		row = [self.name, self.email, self.type, self.value, self.start, 
+		self.end, self.status, self.comment]
+		return row 
+
 		
 
 			
