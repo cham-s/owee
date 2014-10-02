35038ab9c2e7026d5aa48f2a4730511744d3f788
dash-c <deangreyhame@gmail.com>
2014-10-02 16:10:58 +0200

    code cleaning for pep8 style guide

--- dashboard.py
+++ dashboard.py
@@ -2,17 +2,16 @@
 
 import webbrowser
 import item
-import ower 
+import ower
 import time
 from time import gmtime
 import pickle
 import os
 import uuid
-
-
+import re
 
 # declare the dashboard header
-main_page_head ='''
+main_page_head = '''
 <head>
 <meta charset="UTF-8">
 <title>Owee - keep track of them</title>
@@ -22,7 +21,7 @@ main_page_head ='''
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <style type="text/css">
     body{
-    	padding-top: 70px;
+        padding-top: 70px;
     }
 </style>
 <link rel="stylesheet" type="text/css" href="css/dashboard.css">
@@ -64,7 +63,7 @@ main_page_content = '''
               <table class="table table-striped">
                 <thead>
                   <tr>
-                  	<th>Owee ID</th>
+                    <th>Owee ID</th>
                     <th>Name</th>
                     <th>Object Type</th>
                     <th>Value</th>
@@ -90,158 +89,175 @@ main_page_content = '''
 </html>
 '''
 
-# ower row template
+# owee row template
 owee_row = '''
-				<tr>
-					<td>{owee_id}</td>
-					<td>{owee_name}</td>
-					<td>{object_type}</td>
-					<td>${value}</td>
-					<td>{start_date}</td>
-					<td>{end_date}</td>
-					<td>{status}</td>
-					<td>{note}</td>
-				<tr>
-				'''
+                <tr>
+                    <td>{owee_id}</td>
+                    <td>{owee_name}</td>
+                    <td>{object_type}</td>
+                    <td>${value}</td>
+                    <td>{start_date}</td>
+                    <td>{end_date}</td>
+                    <td>{status}</td>
+                    <td>{note}</td>
+                <tr>'''
 
-# return a list of object
+
+# open a file and load object if there are any else return an empty list
 def file_to_list(filename):
-	# open file to read and load objects
-	if os.path.getsize(os.getcwd() + "/" + filename) > 0:
-		f = open(filename, "rb")
-		try:
-			object_list = pickle.load(f)
-			return object_list
-		except EOFError:
-			return None
-		f.close()	
-	else:
-		object_list = []
-		return object_list			
-	
+    # open file to read and load objects
+    if os.path.getsize(os.getcwd() + "/" + filename) > 0:
+        f = open(filename, "rb")
+        try:
+            object_list = pickle.load(f)
+            f.close()
+            return object_list
+        except EOFError:
+            return None
+    else:
+        object_list = []
+        return object_list
 
-# open a file to write and modifies it	
+
+# open a file to write and modifies it
 def modify_file(list_of_object, filename):
-	# open and write objects to file
-	f = open(filename, "wb")
-	pickle.dump(list_of_object, f)
-	f.close()	
+    # open and write objects to file
+    f = open(filename, "wb")
+    pickle.dump(list_of_object, f)
+    f.close()
+
+
+# check user input in "field"
+def check_input(input_type):
+    output = ""
+
+    # check for email input
+    if input_type == "email":
+        output = raw_input("Enter email: ")
+        email_re = re.compile(r"[^@]+@[^@]+\.[^@]+")
+
+        while not email_re.match(output):
+            output = raw_input("Please enter a valid email: ")
+
+    # check for phone number input
+    elif input_type == "phone":
+        output = raw_input("Enter a phone number: ")
+        output = output.replace(" ", "")
+
+        while str.isdigit(output) == False:
+            output = raw_input("Please enter a valid phone number: ")
+            output = output.replace(" ", "")
+    else:
+        print("Argument not valid")
+    return output
+
+
+def match(regex, content):
+    stuff_re = re.compile(regex)
+    if stuff_re.match(content):
+        print "there is a match"
+    else:
+        print "there is no match all"
 
 
 # add an object to this list
 def add_line(filename):
-	first_name = str(raw_input("Enter first name: ")).strip()
-	last_name = str(raw_input("Enter last name: ")).strip()
-	email = str(raw_input("Enter email: ")).strip()
-	address = str(raw_input("Enter adddess: ")).strip()
-	phone = str(raw_input("Enter a phone number: ")).strip()
-	# generate a 5 digit unique id
-	owee_id = str(uuid.uuid4().fields[-1])[:5]
-	item_type = str(raw_input("Enter type: ")).strip()
-	item_value = str(raw_input("Enter value: ")).strip()
-	start_date = time.strftime(" %d %b %Y", gmtime())
-	end_date = str(raw_input("Enter a due date: ")).strip()
-	owee_status = "Still lended"
-	comment = str(raw_input("Enter a note regarding the transaction: ")).strip()
+    first_name = str(raw_input("Enter first name: ")).strip()
+    last_name = str(raw_input("Enter last name: ")).strip()
+    email = str(raw_input("Enter email: ")).strip()
+    address = str(raw_input("Enter adddess: ")).strip()
+    phone = str(raw_input("Enter a phone number: ")).strip()
+    # generate a 5 digit unique id
+    owee_id = str(uuid.uuid4().fields[-1])[:5]
+    item_type = str(raw_input("Enter type: ")).strip()
+    item_value = str(raw_input("Enter value: ")).strip()
+    start_date = time.strftime(" %d %b %Y", gmtime())
+    end_date = str(raw_input("Enter a due date: ")).strip()
+    owee_status = "Still lended"
+    comment = str(raw_input("Enter a note regarding the transaction: ")).strip()
 
-	the_ower = ower.Ower(first_name, last_name, email, phone, address)
+    the_ower = ower.Ower(first_name, last_name, email, phone, address)
 
+    item_loaned = item.ItemLoaned(the_ower, owee_id, item_type, item_value, start_date,
+                                  end_date, owee_status, comment)
 
-	item_loaned = item.ItemLoaned(the_ower, owee_id, item_type, item_value, start_date,
-                end_date, owee_status, comment)
+    owees = file_to_list(filename)
+    owees.append(item_loaned)
 
-	owees = file_to_list(filename)
-	owees.append(item_loaned)
+    modify_file(owees, filename)
 
-	modify_file(owees, filename)
-
-
-
-	
 
 def change_state(filename, state):
-	search_id = ""
-	owees = file_to_list(filename)
-	# iterator
-	i = 0
-	id_is_not_there = True
-	# check if id exits and proceed accordingly
-	while id_is_not_there:
-		search_id = str(raw_input("Enter the owee ID: ")).strip()
-		for owee in owees:
-			if search_id == owee.item_id:
-				# option to delete a row
-				if state == "delete":
-					del(owees[i])
-				# option to change owee status
-				elif state == "status":
-					owee.change_status()
-				# option to change the end date	
-				elif state == "date":
-					new_end_date = raw_input("\nEnter new date (format: dd month year): ")
-					owee.change_end_date(new_end_date)
-				else:
-					print ("Invalid state")
-				id_is_not_there = False	
-			i += 1			
-		if id_is_not_there:
-			print("Please enter a valid ID or 'q' to quit. ")	
-		else:
-			print("\n...Operation succeded\n")
-					
-		if search_id == "q":
-				print("\n...Operation canceled.\n")
-				break;
+    owees = file_to_list(filename)
+    # iterator
+    i = 0
+    id_is_not_there = True
+    # check if id exits and proceed accordingly
+    while id_is_not_there:
+        search_id = str(raw_input("Enter the owee ID: ")).strip()
+        for owee in owees:
+            if search_id == owee.item_id:
+                # option to delete a row
+                if state == "delete":
+                    del (owees[i])
+                # option to change owee status
+                elif state == "status":
+                    owee.change_status()
+                # option to change the end date
+                elif state == "date":
+                    new_end_date = raw_input("\nEnter new date (format: dd month year): ")
+                    owee.change_end_date(new_end_date)
+                else:
+                    print ("Invalid state")
+                id_is_not_there = False
+            i += 1
+        if id_is_not_there:
+            print("Please enter a valid ID or 'q' to quit. ")
+        else:
+            print("\n...Operation succeded\n")
 
-	modify_file(owees, filename)
+        if search_id == "q":
+            print("\n...Operation canceled.\n")
+            break
+
+    modify_file(owees, filename)
 
 
 # create a single row fill with dynamic content
 def create_ower_row_content(owees):
-	# create rows content
-	content = ''
-	for owee in owees:
-		# append with previous entry
-		content += owee_row.format(
-		owee_id=owee.item_id,	
-		owee_name=owee.the_ower.first_name,
-		object_type=owee.item_type,
-		value=owee.value,	
-		start_date=owee.start_date,	
-		end_date=owee.end_date,
-		status=owee.status,
-		note=owee.comment
-		) 
-	return content	
+    # create rows content
+    content = ''
+    for owee in owees:
+        # append with previous entry
+        content += owee_row.format(
+            owee_id=owee.item_id,
+            owee_name=owee.the_ower.first_name,
+            object_type=owee.item_type,
+            value=owee.value,
+            start_date=owee.start_date,
+            end_date=owee.end_date,
+            status=owee.status,
+            note=owee.comment
+        )
+    return content
 
-# open a webbrowser with all owees	
+
+# open a webbrowser with all owees
 
 def open_dashboard_page(owees):
-	# create or overwrite the ouput file
-	output_file = open('dashboard.html', 'w')
+    # create or overwrite the ouput file
+    output_file = open('dashboard.html', 'w')
 
-	# replace the placeholder for the owee with dynamic content
-	rendered_content = main_page_content.format(owee_rows=create_ower_row_content(owees))
+    # replace the placeholder for the owee with dynamic content
+    rendered_content = main_page_content.format(owee_rows=create_ower_row_content(owees))
 
-	#output the file
-	output_file.write(main_page_head + rendered_content)
-	output_file.close()
+    # output the file
+    output_file.write(main_page_head + rendered_content)
+    output_file.close()
 
-	#open the output file in the browser
-	url = os.path.abspath(output_file.name)
-	webbrowser.open('file://' + url, new=2) # open in a new tab if possible
-
-
-
-
-  		
-
-
-	
-
-
-
-	
+    #open the output file in the browser
+    url = os.path.abspath(output_file.name)
+    webbrowser.open('file://' + url, new=2)  # open in a new tab if possible
 
 
 
--- launcher.py
+++ launcher.py
@@ -6,86 +6,84 @@
 
 import dashboard
 
-
-
 # menu user input choice
 choice = 0
 
-# keep the app in a loop untill the user decides to quit
+# keep the app in a loop until the user decides to quit
 while int(choice) < 7:
-	# prompt the user some kind of "UI" menu
-	menu = """
-	| ************************* Welcome to Owee ************************** |
-	                                                                      
-				What would you like to do?
+    # prompt the user some kind of "UI" menu
+    menu = '''
+    | ************************* Welcome to Owee ************************** |
 
-	1. Add an owee
-	2. Delete an owee
-	3. Display Dashboard
-	4. Change the status (reverse the current status Still Loaned/Given Back)
-	5. Send a reminder message to the person owing you
-	6. Modify due date
-	7. Quit
-	> """
+                What would you like to do?
 
-	# check for input validation from choices input
-	try:
-		# declare choice as input
-		choice = int(raw_input(menu))
-		if choice == 1:
-			# add a row to the dashboard
-			print """ 
+    1. Add an owee
+    2. Delete an owee
+    3. Display Dashboard
+    4. Change the status (reverse the current status Still Loaned/Given Back)
+    5. Send a reminder message to the person owing you
+    6. Modify due date
+    7. Quit
+    > '''
 
-			Fill the form, enter 'none' if you don't want to fill an anwser
+    # check for input validation from choices input
+    try:
+        # declare choice as input
+        choice = int(raw_input(menu))
+        if choice == 1:
+            # add a row to the dashboard
+            print """
 
-			 """
-			print("1- Add an owee.\n") 
-			dashboard.add_line("owees.txt")
-			print("\n...0wee successfully added.\n")
+            Fill the form, enter 'none' if you don't want to fill an anwser
 
-		elif choice == 2:
-			# delete a row from the dashboard
-			option = raw_input("Do you really want to delete the owee? There is no going back [y/n]: ")
-			print("\n2- Delete an owee.\n") 
+             """
+            print("1- Add an owee.\n")
+            dashboard.add_line("owees.txt")
+            print("\n...0wee successfully added.\n")
 
-			# agree to delete owee
-			if option == "y":
-				dashboard.change_state("owees.txt", "delete")
+        elif choice == 2:
+            # delete a row from the dashboard
+            option = raw_input("Do you really want to delete the owee? There is no going back [y/n]: ")
+            print("\n2- Delete an owee.\n")
 
-			# refuse to delete owee	
-			elif option == "n":	
-				print("\n...Operation canceled.\n")
+            # agree to delete owee
+            if option == "y":
+                dashboard.change_state("owees.txt", "delete")
 
-			else:
-				print("Please type y for yes or n for no.")		 
+            # refuse to delete owee
+            elif option == "n":
+                print("\n...Operation canceled.\n")
 
-		elif choice == 3:
-			# display all owees in a webpage form
-			owees = dashboard.file_to_list("owees.txt")
-			dashboard.open_dashboard_page(owees)
+            else:
+                print("Please type y for yes or n for no.")
 
-		elif choice == 4:
-			# change the status of an owee
-			dashboard.change_state("owees.txt", "status")
+        elif choice == 3:
+            # display all owees in a webpage form
+            owees = dashboard.file_to_list("owees.txt")
+            dashboard.open_dashboard_page(owees)
 
-		elif choice == 5:
-			# send a message 
+        elif choice == 4:
+            # change the status of an owee
+            dashboard.change_state("owees.txt", "status")
 
-			print("TO DO: implement this")
-			#dashboard.send_message()
-		elif choice == 6:
-			# change the due date
-			dashboard.change_state("owees.txt", "date")
+        elif choice == 5:
+            # send a message
+
+            print("TO DO: implement this")
+            #dashboard.send_message()
+        elif choice == 6:
+            # change the due date
+            dashboard.change_state("owees.txt", "date")
 
 
-		elif choice == 7:
-			# display a message before quitting the app
-			print("\nBye... untill next time\n")	
-		else:
-			# if choice is not valid prompt this error message
-			print("\nSorry but the choice has to be either 1, 2, 3, 4, 5 or 6\n")
-			choice = 0
+        elif choice == 7:
+            # display a message before quitting the app
+            print("\nBye... until next time\n")
+        else:
+            # if choice is not valid prompt this error message
+            print("\nSorry but the choice has to be either 1, 2, 3, 4, 5 or 6\n")
+            choice = 0
 
-	except Exception, e:
-		print str(e)
-		print ("\nPlease enter a number as input\n")						
+    except Exception, e:
+        print str(e)
+        print ("\nPlease enter a number as input\n")
